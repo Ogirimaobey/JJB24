9c659c6c5dbe856249fdd1da277cb6f94b8786dc
Ogirimaobey <ogirimaobey@gmail.com>
2025-11-10 16:00:10 +0100

    Create vip.js

---
+++ vip.js
@@ -0,0 +1,39 @@
+// This file holds the data for the "VIP" page.
+// We are using placeholder images from placehold.co with a different color.
+
+const vipProducts = [
+    { 
+        id: 'vip1', 
+        name: 'CASPERVIP1', 
+        price: 500000, 
+        total_return: 600000, 
+        duration: 30,
+        itemimage: 'https://placehold.co/300x200/1a1a1a/ffffff?text=CASPERVIP1'
+    },
+    { 
+        id: 'vip2', 
+        name: 'CASPERVIP2', 
+        price: 1000000, 
+        total_return: 1200000, 
+        duration: 30,
+        itemimage: 'https://placehold.co/300x200/1a1a1a/ffffff?text=CASPERVIP2'
+    },
+    { 
+        id: 'vip3', 
+        name: 'CASPER3', 
+        price: 2000000, 
+        total_return: 2400000, 
+        duration: 30,
+        itemimage: 'https://placehold.co/300x200/1a1a1a/ffffff?text=CASPER3'
+    },
+    { 
+        id: 'vip4', 
+        name: 'CASPER4', 
+        price: 3000000, 
+        total_return: 3600000, 
+        duration: 30,
+        itemimage: 'https://placehold.co/300x200/1a1a1a/ffffff?text=CASPER4'
+    }
+];
+
+export default vipProducts;
