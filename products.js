52c46ddeb7b03b2a75696ecf2a19863077317084
Ogirimaobey <ogirimaobey@gmail.com>
2025-11-11 03:29:24 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,97 @@
+// This file holds the data for the main "Products" page.
+// We are using placeholder images from placehold.co
+
+const swProducts = [
+    { 
+        id: 'sw1', 
+        name: 'SW1. CHAMDOR', 
+        price: 8000, 
+        validDays: 35, 
+        dailyincome: 400, 
+        totalRevenue: 14000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW1.+CHAMDOR'
+    },
+    { 
+        id: 'sw2', 
+        name: 'SW2. ASCONIAGOR', 
+        price: 15000, 
+        validDays: 35, 
+        dailyincome: 800, 
+        totalRevenue: 28000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW2.+ASCONIAGOR'
+    },
+    { 
+        id: 'sw3', 
+        name: 'SW3. 4thSTREET', 
+        price: 40000, 
+        validDays: 40, 
+        dailyincome: 1600, 
+        totalRevenue: 64000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW3.+4thSTREET'
+    },
+    { 
+        id: 'sw4', 
+        name: 'SW4. CARLO ROSSI', 
+        price: 80000, 
+        validDays: 40, 
+        dailyincome: 3200, 
+        totalRevenue: 128000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW4.+CARLO+ROSSI'
+    },
+    { 
+        id: 'sw5', 
+        name: 'SW5. SANDEMAN RUBY PORTE', 
+        price: 120000, 
+        validDays: 50, 
+        dailyincome: 3800, 
+        totalRevenue: 190000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW5.+SANDEMAN'
+    },
+    { 
+        id: 'sw6', 
+        name: 'SW6. VODKA', 
+        price: 150000, 
+        validDays: 50, 
+        dailyincome: 4400, 
+        totalRevenue: 220000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW6.+VODKA'
+    },
+    { 
+        id: 'sw7', 
+        name: 'SW7. ANDRE', 
+        price: 200000, 
+        validDays: 60, 
+        dailyincome: 4900, 
+        totalRevenue: 294000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW7.+ANDRE'
+    },
+    { 
+        id: 'sw8', 
+        name: 'SW8. IRISH CREAM', 
+        price: 300000, 
+        validDays: 90, 
+        dailyincome: 6000, 
+        totalRevenue: 540000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW8.+IRISH+CREAM'
+    },
+    { 
+        id: 'sw9', 
+        name: 'SW9. GLENFFIDDICH', 
+        price: 400000, 
+        validDays: 90, 
+        dailyincome: 7800, 
+        totalRevenue: 702000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW9.+GLENFFIDDICH'
+    },
+    { 
+        id: 'sw10', 
+        name: 'SW10. MATTEL', 
+        price: 500000, 
+        validDays: 90, 
+        dailyincome: 9800, 
+        totalRevenue: 882000,
+        itemimage: 'https://placehold.co/300x200/6a0dad/ffffff?text=SW10.+MATTEL'
+    }
+];
+
+export default swProducts;
